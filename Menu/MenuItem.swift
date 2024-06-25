8719b1d562448cc219934f4931271793e19a60b5
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 08:27:50 -0300

    feat: add assets to project

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Tauan Tathiell Camargo on 25/06/24.
+//
+
+import Foundation
---
+++ Menu/MenuItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemRow.swift
+//  Menu
+//
+//  Created by Tauan Tathiell Camargo on 25/06/24.
+//
+
+import SwiftUI
+
+struct MenuItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemRow()
+}
