e622091d96cfb97e42ee10513a4019850e892ab2
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 00:05:02 -0300

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Tauan Tathiell Camargo on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
